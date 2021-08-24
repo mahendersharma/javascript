a303329006dea5303efb0104f66d55663ffd00b8
mahendersharma <sharma27july1999@gmail.com>
2021-08-24 11:35:10 +0530

    lec 2.3

---
+++ javascript02/index.js
@@ -0,0 +1,21 @@
+//  2.3
+
+
+// functions 
+// function logger(){
+//     console.log('my name is mahender sharma')
+// }
+
+// // calling   //  rungiing 
+// logger();
+// logger();
+// function fruit(apples, oranges){
+//     console.log(apples, oranges);
+//     const juice = `juice with ${apples} apples and ${oranges} oranges`;
+//     return juice;
+// }
+
+// const juicepro = fruit(5,0);
+// console.log(juicepro);
+// const juiceproa = fruit(2,8);
+// console.log(juiceproa);
